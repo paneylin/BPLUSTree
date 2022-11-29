3afb4e290db1f50c5dd7c0305a426d95bfbcc9d9
paneylin <81309871@qq.com>
2022-11-29 17:21:37 +0800

    modify

--- vanEmdeBoasTree.c
+++ vanEmdeBoasTree.c
@@ -38,6 +38,19 @@ const int DEFAULT_DIVICE_NUM = 2;
 const int MIN_MEMORRY_SIZE = 16;
 const int DATA_BLOCK_SIZE = 4096;
 
+//创建一个vanEmdeBoasTree，传入u值大小范围，传入获取key的函数，key为整数
+vanEmdeBoasTree *createVanEmdeBoasTree(int number , int (*getKeyFunc)(void *));
+//找到key对应的数据，没有返回null
+void *findDataInVEBTree(int key , vanEmdeBoasTree * tree);
+//插入数据，如果数据已经存在，返回，数据key不能冲突
+void insertDataVEBTree(void *data , vanEmdeBoasTree *tree);
+//获取当前比输入key大的第一个数据，key所代表数据可以不存在
+void * getNextDataVEBTree(int key , vanEmdeBoasTree * tree);
+//获取输入key小的第一个数据，key所代表数据可以不存在
+void * getPreDataVEBTree(int key , vanEmdeBoasTree * tree);
+//删除key所代表数据，返回删除的数据，如果不存在，返回null
+void *deleteDataVEBTree(int key , vanEmdeBoasTree * tree);
+
 void destory_node_VEBTree(NodeVanEmdeBoasTree * node){
     if(node == NULL){
         return;
@@ -140,6 +153,15 @@ vanEmdeBoasTree *createVanEmdeBoasTree(int number , int (*getKeyFunc)(void *)){
     tree->getKeyFunc = getKeyFunc;
     return tree;
 } 
+
+void *delete_data_from_dataBlock_VEBTree(int key , vanEmdeBoasTree * tree){
+    int blockIndex = key/DATA_BLOCK_SIZE;
+    void ** dataBlock =  getElementByIndexLList(blockIndex, tree->data->dataList);
+    int lowIndex = key % DATA_BLOCK_SIZE;
+    void * data = dataBlock[lowIndex];
+    dataBlock[lowIndex] = NULL;
+    return data;
+}
 
 void *findDataInVEBTree(int key , vanEmdeBoasTree * tree){
     int blockIndex = key/DATA_BLOCK_SIZE;
@@ -390,6 +412,8 @@ int delete_data_from_tree_VEBTree(int key , NodeVanEmdeBoasTree *node){
     }
     return 1;
 }
+
+
 
 void *deleteDataVEBTree(int key , vanEmdeBoasTree * tree){
     if(key < 0 || key >= tree->dataSize){
